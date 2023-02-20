6f22b6d22e90fa942942954c6834d3ed476c4345
Joperes-ym <joanapereira.ym@gmail.com>
2023-02-20 15:48:32 +0000

    px values changed and semantic naming

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 
 // Get the modal
-let modal = document.getElementById("modal");
+let instructionModal = document.getElementById("instructionModal");
 
 // Get the button that opens the modal
-let btn = document.getElementById("myBtn");
+let instructions = document.getElementById("instructions");
 
 // Get the <span> element that closes the modal
-let span = document.getElementById("close");
+let instructionModalClose = document.getElementById("instructionModalClose");
 
 // When the user clicks on the button, open the modal
-btn.addEventListener('click',() =>{
-    modal.style.display = "block";
+instructions.addEventListener('click',() =>{
+    instructionModal.style.display = "block";
 })
 
 // When the user clicks on <span> (x), close the modal
-span.addEventListener('click',() =>{
-      modal.style.display = "none";
+instructionModalClose.addEventListener('click',() =>{
+    instructionModal.style.display = "none";
   })
 
 // When the user clicks anywhere outside of the modal, close it
 window.addEventListener('click',(e) => {
-    if (e.target == modal) {
-        modal.style.display = "none";
+    if (e.target == instructionModal) {
+        instructionModal.style.display = "none";
     }
 })
 
