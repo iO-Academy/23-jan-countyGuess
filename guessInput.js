e1ed1b2767f053a874f525f1345b5f788c22b9d0
HadertS <source.zero@googlemail.com>
2023-02-21 16:43:18 +0000

    refactor for guess logic to remove array datatype

--- guessInput.js
+++ guessInput.js
@@ -5,18 +5,13 @@ let makeUpper = (values) => {
 
 //handles key presses in the guess box
 const guessHandler = () => {
-    let guessInputArray = [guessInput.value]
-    let upperGuessInput = guessInputArray.map(makeUpper)
-//filter function loops through user guesses and compares to counties list. Adds to new array if correct.
-    let correctInputArray = (upperGuessInput.filter((answer) => { return upperCounties.includes(answer) }))
-//if correctInputArray has a value and is not duplicated, will be added to correctAnswers array
-    if (correctInputArray.length !== 0 && !correctAnswers.includes(correctInputArray[0])){
-        correctAnswers = correctAnswers.concat(correctInputArray)
-        upperGuessInput.forEach((item) => {
-            const listItem = document.createElement("li")
-            listItem.innerText = item
-            answersContainer.appendChild(listItem)
-        })
+    let getInputValue = guessInput.value.toUpperCase()
+
+    if (upperCounties.includes(getInputValue) && !correctAnswers.includes(getInputValue)){
+        correctAnswers.push(getInputValue)
+        const listItem = document.createElement("li")
+        listItem.innerText = getInputValue
+        answersContainer.appendChild(listItem)
         guessInput.value = ''
     }
 }
