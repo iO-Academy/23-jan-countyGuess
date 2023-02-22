fce2da89a89b9b01df6fc4831c10f6e0fc6dbcc3
Steph-ski <stephaniejohns57@gmail.com>
2023-02-22 11:09:42 +0000

    endGame code added

--- index.js
+++ index.js
@@ -26,3 +26,29 @@ window.addEventListener('click',(e) => {
 })
 
 
+// Get the score modal
+let scoreModal = document.getElementById("scoreModal")
+let scoreModalClose = document.querySelector('#scoreModalClose')
+let image = document.querySelector('img')
+let finalScore = document.querySelector('#finalScore')
+
+
+// Temporary event listener until merging with timer function.When the user clicks on the image, open the modal
+image.addEventListener('click',() =>{
+    scoreModal.style.display = "block";
+    document.querySelector('#guessInput').disabled = true
+    document.querySelector('#guessInput').value = ''
+    finalScore.innerText = scoreCount
+})
+
+// When the user clicks on <span> (x), close the modal
+scoreModalClose.addEventListener('click',() =>{
+    scoreModal.style.display = "none";
+})
+
+//hard coded final score until merged
+let scoreCount = 10
+
+
+
+
